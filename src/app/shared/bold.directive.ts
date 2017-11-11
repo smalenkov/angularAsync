2e46af242f08419031ebf9e83978a8fdea916e2f
Vladimir Smalenkov <smalenkoff@gmail.com>
2017-11-11 22:01:38 +0300

    add attr directive

--- src/app/shared/bold.directive.ts
+++ src/app/shared/bold.directive.ts
@@ -1,32 +1,45 @@
-import {Directive, ElementRef, OnInit, Renderer2, HostListener} from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBold]'
 })
 export class BoldDirective implements OnInit {
-    constructor(
-    private el: ElementRef,
-    private renderer: Renderer2) {}
 
-    ngOnInit() {
-      const div = this.renderer.createElement('div');
-      const text = this.renderer.createText('Йух-ууу!');
+  @Input() selectedFontSize;
+  @Input() defaultFontSize;
+  private fontSize: string;
+  private defaultColor: string;
 
-      this.renderer.appendChild(div, text);
-      this.renderer.appendChild(this.el.nativeElement, div);
-      this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
-      this.renderer.setStyle(this.el.nativeElement, 'font-weight', 'bold');
+  constructor(private el: ElementRef,
+              private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.fontSize = this.defaultFontSize;
+    this.defaultColor = '#b7b7b7';
+    const div = this.renderer.createElement('div');
+    const text = this.renderer.createText('Этот блок и текст были запилены средствами Angular! Ох**ть...');
+
+    this.renderer.appendChild(div, text);
+    this.renderer.appendChild(this.el.nativeElement, div);
+    this.renderer.setStyle(this.el.nativeElement, 'cursor', 'pointer');
+    this.renderer.setStyle(this.el.nativeElement, 'color', this.defaultColor);
+  }
+
+  @HostBinding('style.fontSize') get getFontSize() {
+    return this.fontSize;
   }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.setFontWeight('16px');
+    this.setColor('#000000');
+    this.fontSize = this.selectedFontSize;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.setFontWeight('14px');
+    this.setColor('#b7b7b7');
+    this.fontSize = this.defaultFontSize;
   }
 
-  private setFontWeight(val: string) {
-    this.renderer.setStyle(this.el.nativeElement, 'font-size', val);
+  private setColor(color: string) {
+    this.renderer.setStyle(this.el.nativeElement, 'color', color);
   }
 }
