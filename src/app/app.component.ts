86c6c9708c7ac6c5686ef7be6148da1933fa5ccd
Vladimir Smalenkov <smalenkoff@gmail.com>
2017-11-17 05:17:09 +0300

    add eCharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import eCharts from '../../node_modules/echarts';
+import eChartPie from '../../node_modules/echarts/lib/chart/pie';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Async';
-  description = 'Асинхронные запросы и RxJs';
+export class AppComponent implements OnInit {
+  title: string;
+  description: string;
+
+  constructor() {
+    this.title = 'Async';
+    this.description = 'Асинхронные запросы и RxJs';
+  }
+
+  ngOnInit() {
+    this.drawEchart();
+    console.log(eChartPie);
+  }
+
+  drawEchart() {
+    const chartEl = document.getElementById('eChartsBox');
+
+    eCharts.init(chartEl).setOption({
+      series: {
+        type: 'pie',
+        data: [
+          {name: 'A', value: 1212},
+          {name: 'B', value: 2323},
+          {name: 'C', value: 1919}
+        ]
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BoldDirective } from './shared/bold.directive';
+import { ChartsComponent } from './charts/charts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoldDirective
+    BoldDirective,
+    ChartsComponent
   ],
   imports: [
     BrowserModule
